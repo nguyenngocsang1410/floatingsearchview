8211d8c1503af7c44a7c1a5d6a46780bccba1824
nguyenngocsang1410 <nguyenngocsang1410@gmail.com>
2022-11-14 12:47:10 +0700

    Fork

--- sample/src/main/java/com/arlib/floatingsearchviewdemo/MainActivity.java
+++ sample/src/main/java/com/arlib/floatingsearchviewdemo/MainActivity.java
@@ -17,18 +17,20 @@ package com.arlib.floatingsearchviewdemo;
  */
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import com.arlib.floatingsearchview.FloatingSearchView;
 import com.arlib.floatingsearchviewdemo.fragment.BaseExampleFragment;
 import com.arlib.floatingsearchviewdemo.fragment.ScrollingSearchExampleFragment;
 import com.arlib.floatingsearchviewdemo.fragment.SlidingSearchResultsExampleFragment;
 import com.arlib.floatingsearchviewdemo.fragment.SlidingSearchViewExampleFragment;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.List;
 
@@ -58,7 +60,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        List fragments = getSupportFragmentManager().getFragments();
+        List<Fragment> fragments = getSupportFragmentManager().getFragments();
         BaseExampleFragment currentFragment = (BaseExampleFragment) fragments.get(fragments.size() - 1);
 
         if (!currentFragment.onActivityBackPress()) {
--- sample/src/main/java/com/arlib/floatingsearchviewdemo/fragment/ScrollingSearchExampleFragment.java
+++ sample/src/main/java/com/arlib/floatingsearchviewdemo/fragment/ScrollingSearchExampleFragment.java
@@ -2,29 +2,24 @@ package com.arlib.floatingsearchviewdemo.fragment;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.AppBarLayout;
-import android.support.v4.content.res.ResourcesCompat;
 import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+
 import com.arlib.floatingsearchview.FloatingSearchView;
-import com.arlib.floatingsearchview.suggestions.SearchSuggestionsAdapter;
 import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
 import com.arlib.floatingsearchview.util.Util;
 import com.arlib.floatingsearchviewdemo.R;
 import com.arlib.floatingsearchviewdemo.data.ColorSuggestion;
-import com.arlib.floatingsearchviewdemo.data.ColorWrapper;
 import com.arlib.floatingsearchviewdemo.data.DataHelper;
-
-import java.util.List;
+import com.google.android.material.appbar.AppBarLayout;
 
 
 public class ScrollingSearchExampleFragment extends BaseExampleFragment implements AppBarLayout.OnOffsetChangedListener {
@@ -50,10 +45,10 @@ public class ScrollingSearchExampleFragment extends BaseExampleFragment implemen
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mSearchView = (FloatingSearchView) view.findViewById(R.id.floating_search_view);
-        mAppBar = (AppBarLayout) view.findViewById(R.id.appbar);
+        mSearchView = view.findViewById(R.id.floating_search_view);
+        mAppBar = view.findViewById(R.id.appbar);
 
         mAppBar.addOnOffsetChangedListener(this);
 
@@ -62,42 +57,34 @@ public class ScrollingSearchExampleFragment extends BaseExampleFragment implemen
     }
 
     private void setupSearchBar() {
-        mSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+        mSearchView.setOnQueryChangeListener((oldQuery, newQuery) -> {
 
-            @Override
-            public void onSearchTextChanged(String oldQuery, final String newQuery) {
+            if (!oldQuery.equals("") && newQuery.equals("")) {
+                mSearchView.clearSuggestions();
+            } else {
 
-                if (!oldQuery.equals("") && newQuery.equals("")) {
-                    mSearchView.clearSuggestions();
-                } else {
+                //this shows the top left circular progress
+                //you can call it where ever you want, but
+                //it makes sense to do it when loading something in
+                //the background.
+                mSearchView.showProgress();
 
-                    //this shows the top left circular progress
-                    //you can call it where ever you want, but
-                    //it makes sense to do it when loading something in
-                    //the background.
-                    mSearchView.showProgress();
+                //simulates a query call to a data source
+                //with a new query.
+                DataHelper.findSuggestions(getActivity(), newQuery, 5,
+                        FIND_SUGGESTION_SIMULATED_DELAY, results -> {
 
-                    //simulates a query call to a data source
-                    //with a new query.
-                    DataHelper.findSuggestions(getActivity(), newQuery, 5,
-                            FIND_SUGGESTION_SIMULATED_DELAY, new DataHelper.OnFindSuggestionsListener() {
+                            //this will swap the data and
+                            //render the collapse/expand animations as necessary
+                            mSearchView.swapSuggestions(results);
 
-                                @Override
-                                public void onResults(List<ColorSuggestion> results) {
-
-                                    //this will swap the data and
-                                    //render the collapse/expand animations as necessary
-                                    mSearchView.swapSuggestions(results);
-
-                                    //let the users know that the background
-                                    //process has completed
-                                    mSearchView.hideProgress();
-                                }
-                            });
-                }
-
-                Log.d(TAG, "onSearchTextChanged()");
+                            //let the users know that the background
+                            //process has completed
+                            mSearchView.hideProgress();
+                        });
             }
+
+            Log.d(TAG, "onSearchTextChanged()");
         });
 
         mSearchView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
@@ -106,13 +93,8 @@ public class ScrollingSearchExampleFragment extends BaseExampleFragment implemen
 
                 ColorSuggestion colorSuggestion = (ColorSuggestion) searchSuggestion;
                 DataHelper.findColors(getActivity(), colorSuggestion.getBody(),
-                        new DataHelper.OnFindColorsListener() {
-
-                            @Override
-                            public void onResults(List<ColorWrapper> results) {
-                                //show search results
-                            }
-
+                        results -> {
+                            //show search results
                         });
                 Log.d(TAG, "onSuggestionClicked()");
 
@@ -124,13 +106,8 @@ public class ScrollingSearchExampleFragment extends BaseExampleFragment implemen
                 mLastQuery = query;
 
                 DataHelper.findColors(getActivity(), query,
-                        new DataHelper.OnFindColorsListener() {
-
-                            @Override
-                            public void onResults(List<ColorWrapper> results) {
-                                 //show search results
-                            }
-
+                        results -> {
+                             //show search results
                         });
                 Log.d(TAG, "onSearchAction()");
             }
@@ -162,42 +139,33 @@ public class ScrollingSearchExampleFragment extends BaseExampleFragment implemen
 
         //handle menu clicks the same way as you would
         //in a regular activity
-        mSearchView.setOnMenuItemClickListener(new FloatingSearchView.OnMenuItemClickListener() {
-            @Override
-            public void onActionMenuItemSelected(MenuItem item) {
+        mSearchView.setOnMenuItemClickListener(item -> {
 
-                if (item.getItemId() == R.id.action_change_colors) {
+            if (item.getItemId() == R.id.action_change_colors) {
 
-                    mIsDarkSearchTheme = true;
+                mIsDarkSearchTheme = true;
 
-                    //demonstrate setting colors for items
-                    mSearchView.setBackgroundColor(Color.parseColor("#787878"));
-                    mSearchView.setViewTextColor(Color.parseColor("#e9e9e9"));
-                    mSearchView.setHintTextColor(Color.parseColor("#e9e9e9"));
-                    mSearchView.setActionMenuOverflowColor(Color.parseColor("#e9e9e9"));
-                    mSearchView.setMenuItemIconColor(Color.parseColor("#e9e9e9"));
-                    mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
-                    mSearchView.setClearBtnColor(Color.parseColor("#e9e9e9"));
-                    mSearchView.setDividerColor(Color.parseColor("#BEBEBE"));
-                    mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
-                } else {
+                //demonstrate setting colors for items
+                mSearchView.setBackgroundColor(Color.parseColor("#787878"));
+                mSearchView.setViewTextColor(Color.parseColor("#e9e9e9"));
+                mSearchView.setHintTextColor(Color.parseColor("#e9e9e9"));
+                mSearchView.setActionMenuOverflowColor(Color.parseColor("#e9e9e9"));
+                mSearchView.setMenuItemIconColor(Color.parseColor("#e9e9e9"));
+                mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
+                mSearchView.setClearBtnColor(Color.parseColor("#e9e9e9"));
+                mSearchView.setDividerColor(Color.parseColor("#BEBEBE"));
+                mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
+            } else {
 
-                    //just print action
-                    Toast.makeText(getActivity().getApplicationContext(), item.getTitle(),
-                            Toast.LENGTH_SHORT).show();
-                }
-
+                //just print action
+                Toast.makeText(getActivity().getApplicationContext(), item.getTitle(),
+                        Toast.LENGTH_SHORT).show();
             }
+
         });
 
         //use this listener to listen to menu clicks when app:floatingSearch_leftAction="showHome"
-        mSearchView.setOnHomeActionClickListener(new FloatingSearchView.OnHomeActionClickListener() {
-            @Override
-            public void onHomeClicked() {
-
-                Log.d(TAG, "onHomeClicked()");
-            }
-        });
+        mSearchView.setOnHomeActionClickListener(() -> Log.d(TAG, "onHomeClicked()"));
 
         /*
          * Here you have access to the left icon and the text of a given suggestion
@@ -210,33 +178,28 @@ public class ScrollingSearchExampleFragment extends BaseExampleFragment implemen
          * Keep in mind that the suggestion list is a RecyclerView, so views are reused for different
          * items in the list.
          */
-        mSearchView.setOnBindSuggestionCallback(new SearchSuggestionsAdapter.OnBindSuggestionCallback() {
-            @Override
-            public void onBindSuggestion(View suggestionView, ImageView leftIcon,
-                                         TextView textView, SearchSuggestion item, int itemPosition) {
-                ColorSuggestion colorSuggestion = (ColorSuggestion) item;
+        mSearchView.setOnBindSuggestionCallback((suggestionView, leftIcon, textView, item, itemPosition) -> {
+            ColorSuggestion colorSuggestion = (ColorSuggestion) item;
 
-                String textColor = mIsDarkSearchTheme ? "#ffffff" : "#000000";
-                String textLight = mIsDarkSearchTheme ? "#bfbfbf" : "#787878";
+            String textColor = mIsDarkSearchTheme ? "#ffffff" : "#000000";
+            String textLight = mIsDarkSearchTheme ? "#bfbfbf" : "#787878";
 
-                if (colorSuggestion.getIsHistory()) {
-                    leftIcon.setImageDrawable(ResourcesCompat.getDrawable(getResources(),
-                            R.drawable.ic_history_black_24dp, null));
+            if (colorSuggestion.getIsHistory()) {
+                leftIcon.setImageDrawable(ResourcesCompat.getDrawable(getResources(),
+                        R.drawable.ic_history_black_24dp, null));
 
-                    Util.setIconColor(leftIcon, Color.parseColor(textColor));
-                    leftIcon.setAlpha(.36f);
-                } else {
-                    leftIcon.setAlpha(0.0f);
-                    leftIcon.setImageDrawable(null);
-                }
-
-                textView.setTextColor(Color.parseColor(textColor));
-                String text = colorSuggestion.getBody()
-                        .replaceFirst(mSearchView.getQuery(),
-                                "<font color=\"" + textLight + "\">" + mSearchView.getQuery() + "</font>");
-                textView.setText(Html.fromHtml(text));
+                Util.setIconColor(leftIcon, Color.parseColor(textColor));
+                leftIcon.setAlpha(.36f);
+            } else {
+                leftIcon.setAlpha(0.0f);
+                leftIcon.setImageDrawable(null);
             }
 
+            textView.setTextColor(Color.parseColor(textColor));
+            String text = colorSuggestion.getBody()
+                    .replaceFirst(mSearchView.getQuery(),
+                            "<font color=\"" + textLight + "\">" + mSearchView.getQuery() + "</font>");
+            textView.setText(Html.fromHtml(text));
         });
     }
 
@@ -251,10 +214,7 @@ public class ScrollingSearchExampleFragment extends BaseExampleFragment implemen
         //to close, then we don't want to close the activity. if mSearchView.setSearchFocused(false)
         //returns false, we know that the search was already closed so the call didn't change the focus
         //state and it makes sense to call supper onBackPressed() and close the activity
-        if (!mSearchView.setSearchFocused(false)) {
-            return false;
-        }
-        return true;
+        return mSearchView.setSearchFocused(false);
     }
 
     private void setupDrawer() {
